d5805372591bb7051302155bac5d8edfc56be5e3
Nail <nailtagiyev1999@gmail.com>
2023-03-07 15:23:33 +0400

    add dependencies

---
+++ src/main/java/om/rabbitmq/tutorial/SpringBootRabbitMQTutorialApplication.java
@@ -0,0 +1,11 @@
+package om.rabbitmq.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRabbitMQTutorialApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRabbitMQTutorialApplication.class, args);
+    }
+}
